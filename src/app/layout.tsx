03466249ee926790b028e381df2791c89a0db0d9
Muhammad Saad <160317082+sahersaleem@users.noreply.github.com>
2024-11-01 12:39:45 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+
 import {Inter} from "next/font/google"
-import { Heebo } from "next/font/google";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
